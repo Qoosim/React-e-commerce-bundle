fa1ff47b535072cf4d309e2518f2a975f3405359
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-04 14:40:25 +0100

    Create template for the e-commerce-site

---
+++ e-commerce-site/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hey you there!</h3>
+      <button className="btn btn-primary">Add me</button>
+    </div>
+  );
+}
+
+export default App;
