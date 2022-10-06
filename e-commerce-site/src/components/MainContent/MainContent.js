165b80213c322f60a4af411e037413830a4c4da4
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-06 02:23:16 +0100

    Create table content with some conditional rendering

--- e-commerce-site/src/components/MainContent/MainContent.js
+++ e-commerce-site/src/components/MainContent/MainContent.js
@@ -4,7 +4,14 @@ class MainContent extends Component {
 
   state = {
     title: "Customers",
-    count: 5
+    count: 5,
+    customers: [
+      { id: 1, name: "James", phone: "123-456", address: { city: "Eleyele" } },
+      { id: 2, name: "John", phone: null, address: { city: "Sango" } },
+      { id: 3, name: "Peter", phone: "113-152", address: { city: "Ojoo" } },
+      { id: 4, name: "Jonas", phone: "143-401", address: { city: "UI" } },
+      { id: 5, name: "Mark", phone: null, address: { city: "Ogunpa" } },
+    ]
   }
 
   onRefreshClick = () => {
@@ -12,6 +19,14 @@ class MainContent extends Component {
       ...this.state,
       count: this.state.count + 1
     })
+  }
+
+  getPhoneNumber = (phone) => {
+    return !phone ? (
+      <div className="bg-warning p-2 text-center">No Phone Number</div>
+    ) : (
+      phone
+    )
   }
   
   render() {
@@ -30,6 +45,31 @@ class MainContent extends Component {
             Refresh
           </button>
         </h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Customer Name</th>
+              <th>Phone Number</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.customers.map((customer) => {
+                const { id, name, phone, address } = customer;
+                return (
+                 <tr key={id}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                   <td>{this.getPhoneNumber(phone)}</td>
+                  <td>{address.city}</td>
+                </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </>
     )
   }
