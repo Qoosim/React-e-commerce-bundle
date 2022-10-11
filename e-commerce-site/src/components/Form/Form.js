bf1cb5ff3a95c74142f21ec3427a4a2f405accad
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-11 21:28:32 +0100

    Add logic to login button

--- e-commerce-site/src/components/Form/Form.js
+++ e-commerce-site/src/components/Form/Form.js
@@ -11,6 +11,10 @@ class Form extends Component {
     }
   }
 
+  handleLoginClick = () => {
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className="col-lg-9">
@@ -18,13 +22,25 @@ class Form extends Component {
 
         <div className="form-group form-row">
           <label className="col-lg-4" htmlFor="email">Email:</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.email}
+            onChange={(e) => { this.setState({ email: e.target.value }) }} 
+          />
         </div>
         <div className="form-group form-row">
           <label className="col-lg-4" htmlFor="password">Password:</label>
-          <input type="password" className="form-control" />
+          <input
+            type="password"
+            className="form-control"
+            value={this.state.password}
+            onChange={(e) => { this.setState({ password: e.target.value }) }}
+          />
         </div>
-
+        <div>
+          <button className="btn btn-primary" onClick={this.handleLoginClick}>Login</button>
+        </div>
       </div>
     )
   }
