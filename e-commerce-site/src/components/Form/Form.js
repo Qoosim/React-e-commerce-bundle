b62aceb84973e2759c1167c29439536dfbc46187
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-13 04:10:46 +0100

    Add logic to the form

--- e-commerce-site/src/components/Form/Form.js
+++ e-commerce-site/src/components/Form/Form.js
@@ -7,20 +7,30 @@ class Form extends Component {
 
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      message: ''
     }
   }
 
   handleLoginClick = () => {
     console.log(this.state)
+    if (this.state.email === 'admin@test.com' && this.state.password === 'admin123') {
+      this.setState({
+        message: <span className="text-success">Successfully logged-in</span>
+      })
+    } else {
+      this.setState({
+        message: <span className="text-danger">Invalid login, please try again!</span>
+      })
+    }
   }
 
   render() {
     return (
-      <div className="col-lg-9">
-        <h4 className="m-1 p-2 border-bottom">Login</h4>
+      <div className="col-lg-4 m-auto">
+        <h4 className="m-4 text-center">Login</h4>
 
-        <div className="form-group form-row">
+        <div className="form-group form-row m-2">
           <label className="col-lg-4" htmlFor="email">Email:</label>
           <input
             type="text"
@@ -29,7 +39,7 @@ class Form extends Component {
             onChange={(e) => { this.setState({ email: e.target.value }) }} 
           />
         </div>
-        <div className="form-group form-row">
+        <div className="form-group form-row m-2">
           <label className="col-lg-4" htmlFor="password">Password:</label>
           <input
             type="password"
@@ -38,8 +48,14 @@ class Form extends Component {
             onChange={(e) => { this.setState({ password: e.target.value }) }}
           />
         </div>
-        <div>
-          <button className="btn btn-primary" onClick={this.handleLoginClick}>Login</button>
+        <div className="float-end">
+          <span className="p-2">{this.state.message}</span>
+          <button
+            className="btn btn-primary"
+            onClick={this.handleLoginClick}
+          >
+            Login
+          </button>
         </div>
       </div>
     )
