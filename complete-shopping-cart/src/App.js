b45889471f4c6917b76a58b189faab183b10f411
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-18 23:51:29 +0100

    Create and style navbar for the app

--- complete-shopping-cart/src/App.js
+++ complete-shopping-cart/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <h3>App Component</h3>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
