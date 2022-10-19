76010d2649ae01ab8e6346517438fce995640e48
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-20 00:25:13 +0100

    Use createAsyncThunk and createSlice from react-toolkit to fetch data from backend

---
+++ complete-shopping-cart/frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
+import NotFound from './components/NotFound/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
