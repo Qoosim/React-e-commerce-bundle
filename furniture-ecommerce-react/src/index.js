96a0cab6d41e0dc8161e83df902666880deac1fc
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-12-19 14:44:26 +0100

    Add redux to manage app state changes and react toastify for operation pop messages

--- furniture-ecommerce-react/src/index.js
+++ furniture-ecommerce-react/src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
+import store from './redux/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import App from "./App";
 
@@ -10,7 +14,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+          />
+          <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
