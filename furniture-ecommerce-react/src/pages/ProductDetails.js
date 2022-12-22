466235b8a625c82ce22726545557208115b7e335
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-12-22 12:27:09 +0100

    Implement logic for productDetails component

--- furniture-ecommerce-react/src/pages/ProductDetails.js
+++ furniture-ecommerce-react/src/pages/ProductDetails.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
+import products from '../assets/data/products';
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/CommonSection';
 
 const ProductDetails = () => {
+
+  const { id } = useParams();
+
+  const product = products.find((product) => product.id === id);
+
+  const { imgUrl, productName, price, review, avgRating, description } = product;
+
   return (
-    <>
-      <h3>ProductDetails</h3>
-    </>
+    <Helmet>
+      <CommonSection />
+      
+      <section className={`pt-0`}>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <img src={imgUrl} alt={productName} />
+            </Col>
+            <Col lg='6'>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
